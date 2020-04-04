664103119010008708483c2d011aa6e28ce6b5d4
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2020-04-04 20:13:16 -0300

    tuplas

--- ls3.py
+++ ls3.py
@@ -11,36 +11,47 @@
 
 # print(a_list)
 
-#Concatenando
-lista1 = [10, 20, 30]
+# #Concatenando
+# lista1 = [10, 20, 30]
 
-lista2 = [40, 50]
+# lista2 = [40, 50]
 
-cat_listas = lista1 + lista2
+# cat_listas = lista1 + lista2
 
-print(cat_listas)
+# print(cat_listas)
 
-for i in range(len(cat_listas)):
-   print(f'{i}: {cat_listas[i]}')
+# for i in range(len(cat_listas)):
+#    print(f'{i}: {cat_listas[i]}')
 
-a = [1, 2, 3]
+# a = [1, 2, 3]
 
-b = [1, 2, 3]
+# b = [1, 2, 3]
 
-c = [1, 2, 3, 4]
+# c = [1, 2, 3, 4]
 
-a == b
-a == c
-a < c
-c >= b
+# a == b
+# a == c
+# a < c
+# c >= b
 
+#Tuplas
 
+student_tuple = 'John', 'Green', 3.3
 
+print(student_tuple[2] * 3600, student_tuple[0])
 
+tupla1 = (10, 20, 30)
+tupla2 = tupla1
 
+tupla1 += (20, 20)
 
+print(tupla1, tupla2)
 
+numbers_lista = [1, 2, 3, 4, 5]
 
+numbers_tupla += (6, 7)
+
+student_tu
 
 
 
