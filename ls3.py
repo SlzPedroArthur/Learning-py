df24b5932c93f4d99925e1817106f18fb93062e7
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2020-04-04 20:04:23 -0300

    listas baby

---
+++ ls3.py
@@ -0,0 +1,60 @@
+# #Lists
+
+# c = [-45, 6, 3.4223, 'Smith', 123]
+
+# len(c)
+
+# s = 'Hello' #String imutável
+
+# for number in range(1, 6):
+#    a_list += [number]
+
+# print(a_list)
+
+#Concatenando
+lista1 = [10, 20, 30]
+
+lista2 = [40, 50]
+
+cat_listas = lista1 + lista2
+
+print(cat_listas)
+
+for i in range(len(cat_listas)):
+   print(f'{i}: {cat_listas[i]}')
+
+a = [1, 2, 3]
+
+b = [1, 2, 3]
+
+c = [1, 2, 3, 4]
+
+a == b
+a == c
+a < c
+c >= b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
