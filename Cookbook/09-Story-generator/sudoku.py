02be5cf7d65829437c362fb635c2d76117cbb3e2
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2020-04-15 18:17:56 -0300

    que backrtracking lindo

--- Cookbook/09-Story-generator/sudoku.py
+++ Cookbook/09-Story-generator/sudoku.py
@@ -11,24 +11,41 @@ grid = [
    [0,0,0,4,1,9,0,0,5],
    [0,0,0,0,8,0,0,7,9]
 ]
+import numpy as np
 
+print(np.matrix(grid))
 def possible(x, y, n):
    global grid
 
-   j = 0
    for i in range(9):
-      if grid[i][j] == n:
+      if grid[i][y] == n:
          return False
-      for j in range(9):
-         if grid[x][y] == n:
-            return False
+   
+   for i in range(9):
+      if grid[x][i] == n:
+         return False
+   
    x0 = (x // 3) * 3
    y0 = (y // 3) * 3
 
-   for x0 in range(3):
-      for y0 in range(3):
+   for i in range(3):
+      for j in range(3):
          if grid[x0][y0] == n:
             return False
    return True
 
-print(possible(4, 4, 5))
+def solve():
+   global grid
+
+   for i in range(1,10):
+      for j in range(1,10):
+         if grid[i][j] == 0:
+            for n in range(1,9):
+               if possible(i, j, n):
+                  grid[i][j] = n
+                  solve()
+                  grid[i][j] = 0 #Isso é muito lindo
+            return
+   print(np.matrix(grid))
+   input('More?')
+                  
