5ec2dfc92acf0562432deefb15443cae080c9f82
Pedro Arthur <slzpedroarthurcp@protonmail.com>
2020-04-28 20:15:52 -0300

    jeopardy game done!

--- Cookbook/15-jeopardy/game_jeopardy.py
+++ Cookbook/15-jeopardy/game_jeopardy.py
@@ -3,19 +3,43 @@ import sqlite3
 c = sqlite3.connect('jeopardy.db')
 cursor = c.cursor()
 
-cursor.execute("SELECT game FROM category ORDER BY RANDOM() LIMIT 1")
-result = cursor.fetchall()
-game_id = result[0][0]
+def queryGame():
+   cursor.execute('SELECT game FROM category ORDER BY RANDOM() LIMIT 1')
+   result = cursor.fetchall()
+   game_id = result[0][0]
+   
+   cursor.execute('SELECT name FROM category WHERE game = %d' % (game_id,))
+   result = cursor.fetchall()
+   name = result[0][0]
 
-print('Cagories for game #%d' % (game_id))
+   query = 'SELECT text, answer, value FROM clue WHERE game = %d' % (game_id,)
+   cursor.execute(query)
+   result = cursor.fetchall()
+   
+   return (game_id, name, result)
+   
+game_id, name, result = queryGame()
 
-query = """SELECT name, round FROM category
-WHERE game = %d ORDER BY round""" % (game_id,)
-cursor.execute(query)
-results = cursor.fetchall()
+print('======>Joepardy GAME<=====')
+print('Primeira pergunta!')
 
-for result in results:
-   name, round = result
-   print('Round %d: %s' % (round, name))
+for game_round in result:
+   text, answer, value = game_round
+   print('Pergunta: %s' % (text,))
+   print(answer)
+   
+   op = 'n'
+   while op == 'n':
+      user_answer = str(input('Sua resposta: '))
+      op = str(input("Tem certeza que '%s' é a resposta? (y/n)"))
+
+   if user_answer == answer:
+      print('Certa resposta!')
+      continue
+   else:
+      print('PERDEU!')
+      break
+
+print(text)   
 
 c.close()
